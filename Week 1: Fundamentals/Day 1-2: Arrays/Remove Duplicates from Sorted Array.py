70aa946ad5d3d2831751cfddca52fe1c53f423d3
AFE John Ibukun <afejohn001@gmail.com>
2025-01-03 14:32:15 +0100

    Remove Duplicates from Sorted Array.py

---
+++ Week 1: Fundamentals/Day 1-2: Arrays/Remove Duplicates from Sorted Array.py
@@ -0,0 +1,20 @@
+"""
+  Assuming nums = [0,0,1,1,1,2,2,3,3,4]
+  The problem states that we should remove the duplicates such that the result remaining in non-descending order.
+  For that we will need to use two pointers, one to iterate through to check 
+  our values and one to serve as a place holder to modify our values.
+  
+"""
+
+
+def removeDuplicates(nums):
+    left = 1
+    for right in range(1, len(nums)):
+        if nums[right] != nums[right - 1]:
+            nums[left] = nums[right]
+            left += 1
+    return left
+
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+print(removeDuplicates(nums))
