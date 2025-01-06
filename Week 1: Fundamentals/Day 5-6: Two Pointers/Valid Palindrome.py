c94fae42acba51a3410fe16fee179b6ba4fbe267
AFE John Ibukun <afejohn001@gmail.com>
2025-01-06 21:20:18 +0100

    Valid Palindrome.py

--- Week 1: Fundamentals/Day 5-6: Two Pointers/Valid Palindrome.py
+++ Week 1: Fundamentals/Day 5-6: Two Pointers/Valid Palindrome.py
@@ -1,1 +1,11 @@
+def validPalindrome(s):
+    newstr = ""
+    for char in s:
+        if char.isalnum():
+            newstr += char.lower()
+    return newstr == newstr[::-1]
+
+
+s = "A man, a plan, a canal: Panama"
+print(validPalindrome(s))
 
