8922c9639ab84676532a51eeea56a444409395ec
AFE John Ibukun <afejohn001@gmail.com>
2025-01-24 21:39:09 +0100

    Subsets.py

--- Week 4: Algorithms & Challenges/Day 23: Backtracking/Subsets.py
+++ Week 4: Algorithms & Challenges/Day 23: Backtracking/Subsets.py
@@ -1,1 +1,20 @@
+def subsets(nums):
+    result = []
+
+    def backtrack(start, current):
+        result.append(current[:])
+
+        for i in range(start, len(nums)):
+            # includes nums[i]
+            current.append(nums[i])
+            backtrack(i + 1, current)
+            # excludes nums[i]
+            current.pop()
+
+    backtrack(0, [])
+    return result
+
+
+nums = [1, 2, 3]
+print(subsets(nums))
 
